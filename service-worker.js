627e3239857ef2b53bb55fe35f3f576128f0e6a5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-14 01:45:15 +0000

    Deploy pakeweb/pakeweb.github.io to github.com/pakeweb/pakeweb.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js");
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "79c060cb04f820c7b98289faf259ed62"
+    "revision": "0d79c51baa5303a29240433ac4a273a5"
   },
   {
     "url": "android-chrome-192x192.png",
@@ -44,36 +44,36 @@ self.__precacheManifest = [
     "revision": "ac4f5fa1c130478bd4b7fad8ea4389da"
   },
   {
-    "url": "assets/js/3.ebd34f67.js",
+    "url": "assets/js/3.f305254c.js",
     "revision": "b7655e97f4338d3e44fbe308b55f5d86"
   },
   {
-    "url": "assets/js/4.8b1fd65d.js",
+    "url": "assets/js/4.6a5efaad.js",
     "revision": "54b38c236d330ce76c407c4101f6d639"
   },
   {
-    "url": "assets/js/5.684b19bb.js",
+    "url": "assets/js/5.2c443da1.js",
     "revision": "456ef8afe352acd520f2f4aadd7d5c27"
   },
   {
-    "url": "assets/js/6.579e5b10.js",
+    "url": "assets/js/6.d09b91b5.js",
     "revision": "ec9431d2814ab5932acce61a0dc0a2aa"
   },
   {
-    "url": "assets/js/7.fd11d87f.js",
+    "url": "assets/js/7.d6a0ba0f.js",
     "revision": "3729ad808ddd4d307d2cc8c3b622c932"
   },
   {
-    "url": "assets/js/8.fa61c755.js",
+    "url": "assets/js/8.a4665063.js",
     "revision": "7a1ce5c4b336b40f6916b440aebd1df0"
   },
   {
-    "url": "assets/js/9.c17ab832.js",
+    "url": "assets/js/9.8ea7262c.js",
     "revision": "db52875170193e9a106dd3814deebfa1"
   },
   {
-    "url": "assets/js/app.ec29913b.js",
-    "revision": "83a0b64f08709a9950f1bb232fc082f9"
+    "url": "assets/js/app.18e8ca4a.js",
+    "revision": "653c22f221240448fe1687c0caa702ee"
   },
   {
     "url": "favicon-16x16.png",
@@ -85,15 +85,15 @@ self.__precacheManifest = [
   },
   {
     "url": "hubungi/index.html",
-    "revision": "5b7239600e01639f3e5ea7cb67b1eb26"
+    "revision": "46c89a55f16487b02e2373a7c455322d"
   },
   {
     "url": "ikhtisar/index.html",
-    "revision": "7012eb4b02be33552a6009dfebc6ddd1"
+    "revision": "76a2dcd8276a2dd992e37abe4b50385c"
   },
   {
     "url": "index.html",
-    "revision": "878c2d53ba85a45d94c4ab3d28fa81a2"
+    "revision": "4bb0a475352f14a9d4ca4eab540ba481"
   },
   {
     "url": "logo.png",
@@ -109,11 +109,11 @@ self.__precacheManifest = [
   },
   {
     "url": "pengajuan/index.html",
-    "revision": "a7b1412c6f09f25a94f1fe33ffd22eca"
+    "revision": "66f2c80f09d7c1c3a93b27a189fa6a8a"
   },
   {
     "url": "pengembang/index.html",
-    "revision": "e06148b1be907712841f9a78c856e8e6"
+    "revision": "6d9d52a061d6c039583d84271cd576cb"
   },
   {
     "url": "safari-pinned-tab.svg",
